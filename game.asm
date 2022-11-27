3601a3d06cf66020451c946c60cc73fe00bca2d7
Hassan <Iceblade765@users.noreply.github.com>
2022-11-27 21:42:08 +0500

    update v4.0

--- game.asm
+++ game.asm
@@ -15,13 +15,14 @@ barLength = 60
 barSpeed = 5
 ;----------ball's variables
 BallStruct struct
-	CoordX dw 50
-	CoordY dw 50
+	CoordX dw 158
+	CoordY dw 180
 	SpeedX dw 5
 	SpeedY dw 5
 BallStruct ends
-;ball variable
+;ball variables
 ball BallStruct 1 dup(<>)
+isBallLaunched db 0
 ;constants
 ballSize = 5
 ;----------- bricks
@@ -38,6 +39,8 @@ brickLoopVar dw 20
 brick BrickStruct    <40,20>, <80+brickGap,20>, <130+brickGap,20>, <180+brickGap,20>, <230+brickGap,20>, 
 	<40,40+brickGap>, <80+brickGap,40+brickGap>, <130+brickGap,40+brickGap>, <180+brickGap,40+brickGap>, <230+brickGap,40+brickGap>,
 	<40,70+brickGap>, <80+brickGap,70+brickGap>, <130+brickGap,70+brickGap>, <180+brickGap,70+brickGap>, <230+brickGap,70+brickGap>
+	
+brokenBricks dw 15 dup(-1)
 ;------------ player
 PlayerStruct struct
 	Score dw 0
@@ -51,9 +54,14 @@ player PlayerStruct <>
 scoreStr db "Score: ",'$'
 nameStr_1 db "Player Name: ", '$'
 nameStr_2 db "Name: ",'$'
-timeVar db 0
+LivesStr db "Lives: ",'$'
+levelStr db "Level: ", '$'
+timeRemaining dw 0
+timeVar_1 db 0
+timeVar_2 db 0
 windowsWidth = 200
 windowsLength = 320
+statsVar dw 0
 count dw 0
 
 
@@ -86,6 +94,15 @@ count dw 0
 		int 10h
 	ENDM
 	
+	addBrokenBrick MACRO si
+		mov di, -1
+		brokenBrickLoop:
+			inc di
+			cmp brokenBricks[di], -1
+		jne brokenBrickLoop
+		mov brokenBricks[di], ax ;(ax has the value of si)
+	ENDM
+	
 	clearBrick MACRO si
 		
 		mov bh, 0; page number
@@ -109,7 +126,31 @@ count dw 0
 		dec brickLoopVar
 		cmp brickLoopVar, 0
 		jne clearBrickLoop_2
-
+	ENDM
+	
+	updateTime MACRO
+		;----------- 4 mins of game time
+		;if
+		cmp timeVar_2, dh
+		je timeSkip
+		;else
+		inc timeRemaining
+		mov timeVar_2, dh
+		;-setting the cursor position
+		mov ah, 02h
+		mov bh, 0
+		mov dh, 1
+		mov dl, 17
+		int 10h
+		
+		mov ax, timeRemaining
+		mov statsVar, ax
+		call updateStats
+		;---
+		cmp timeRemaining, 240
+		je exitGame
+		
+		timeSkip:
 	ENDM
 
 ;------- Attaching the data segment
@@ -131,51 +172,24 @@ mov ah, 4ch
 int 21h
 main endp
 
-;---------------------------------------------------------------------------------------------
-drawBrick proc uses ax bx cx dx si di
-	mov bh, 0; page number
-	mov si, 0
-	mov al, 1
-brickLoop_1:
-		mov cx, brick[si].CoordX ;inital x
-		mov dx, brick[si].CoordY ;inital y
-		mov brickLoopVar, 20
-		brickLoop_2:
-			mov di, brickLength
-			mov cx, brick[si].CoordX
-			brickLoop_3:
-				call drawPixel
-				inc cx
-				
-			dec di
-			cmp di, 0
-			jne brickLoop_3
-			
-			inc dx
-		dec brickLoopVar
-		cmp brickLoopVar, 0
-		jne brickLoop_2
-
-inc al
-add si, type BrickStruct
-cmp si, 60
-jb brickLoop_1
-ret
-drawBrick ENDP
 
 ;---------------------------------------------------------------------------------------------
-game PROC uses ax bx
+game PROC uses ax bx cx dx si di
 
 timeLoop:
 	mov ah, 2ch	;getting the time
 	int 21h
 	
+	updateTime
+
 	;if 1/100 second hasnt passed, repeat loop
-	cmp dl, timeVar
+	cmp dl, timeVar_1
 	je timeLoop
 	
 	;displaying the score
 	call displayStats
+	
+	call brickCollision
 	
 	;moving the ball
 	clearBall
@@ -190,13 +204,17 @@ timeLoop:
 	call moveBar
 	makeBar
 	
+	
+	
 	;Now that 1/100 sec has passed, store the prev second in time var and repeat the loop again
-	mov timeVar, dl
+	mov timeVar_1, dl
 	jmp timeLoop
 	
+	exitGame:
 ret
 game endp
-;----------- 
+;---------------------------------------------------
+;--------------------------------------------------
 inputName proc uses ax bx cx dx
 
 	;setting the cursor position
@@ -252,7 +270,9 @@ displayStats proc uses ax bx dx
 	mov dh, 1
 	mov dl, 8
 	int 10h
-	call displayScore
+	mov ax, player.Score
+	mov statsVar, ax
+	call updateStats
 	
 ;------------ Player Name
 
@@ -276,8 +296,67 @@ displayStats proc uses ax bx dx
 	mov ah, 09h
 	int 21h
 	
+	;------------ Lives
+	call clearHearts
+	
+	mov ah, 02h
+	mov bh, 0
+	mov dh, 24
+	mov dl, 1
+	int 10h
+	
+	mov dx, offset LivesStr
+	mov ah, 09h
+	int 21h
+	
+	
+	mov ah, 02h
+	mov bh, 0
+	mov dh, 24
+	mov dl, 7
+	int 10h
+	
+	cmp player.Lives, 0
+	jne skipskip
+	mov player.Lives, 3
+	
+	skipskip:
+
+	mov cx, player.Lives
+
+displayHearts:
+	mov dl, 3;	displaying hearts
+	mov ah, 02h
+	int 21h
+loop displayHearts
+	
+	;------------- levelStr
+	mov ah, 02h
+	mov bh, 0
+	mov dh, 24
+	mov dl, 32
+	int 10h
+	
+	mov dx, offset levelStr
+	mov ah, 09h
+	int 21h
+	
+	;------------ 
+	
 ret
 displayStats endp
+;-------------------
+clearHearts Proc uses ax bx cx dx
+	mov ah, 06h
+	mov bh, 0 ; black colour
+	mov al, 2
+	mov ch, 23
+	mov dh, 24
+	mov cl, 1
+	mov dl, 9
+	int 10h
+ret
+clearHearts endp
 ;---------------------------------------------------------------------------------------------
 moveBar PROC uses ax bx cx dx
 moveBar_Loop:
@@ -292,13 +371,29 @@ moveBar_Loop:
 	je leftKey
 	cmp ah, 4Dh; right key
 	je rightKey
+	cmp ah, 57 ;space key
+	je launchBall
+	jmp moveBarExit
+	
+	launchBall:
+	mov isBallLaunched, 1
+	jmp moveBarExit
 
 	leftKey:
+	
 		;boundaryCheckLeft
 		cmp bar.CoordX, 0
 		jle moveBar_Loop
 		sub bar.CoordX, barSpeed
+		
+		
+		cmp isBallLaunched, 0
+		jne moveBar_Loop
+		clearBall
+		sub ball.CoordX, barSpeed
+		
 	jmp moveBar_Loop
+	
 
 	rightKey:
 		;boundaryCheckRight
@@ -307,6 +402,12 @@ moveBar_Loop:
 		cmp ax, windowsLength
 		jge moveBar_Loop
 		add bar.CoordX, barSpeed
+		
+		cmp isBallLaunched, 0
+		jne moveBar_Loop
+		clearBall
+		add ball.CoordX, barSpeed
+		
 	jmp moveBar_Loop
 	
 	moveBarExit:
@@ -346,10 +447,15 @@ ret
 drawPixel endp
 ;---------------------------------------------------------------------------------------------
 moveBall PROC uses ax 
+	cmp isBallLaunched, 0
+	je moveBallExit
+	
+	call brickCollision
 	
 	;moving the ball in x-axis
 	mov ax, ball.SpeedX
 	add ball.CoordX, ax
+	
 	
 	;if hit the left wall
 	cmp ball.CoordX, 0
@@ -373,7 +479,7 @@ moveBall PROC uses ax
 	mov ax, ball.CoordY
 	add ax, ballSize
 	cmp ax, windowsWidth
-	jge reverseSpeedY
+	jge youDied
 	
 	;checking ball's collision with bar
 	mov ax, ball.CoordY
@@ -392,8 +498,11 @@ moveBall PROC uses ax
 	cmp ax, bx
 	jl reverseSpeedY
 	
+	;if not collision occurs, simply return
 	moveBallExit:
 	ret 
+	
+	;reversing direction after collision
 	reverseSpeedY:
 	neg ball.SpeedY
 	ret
@@ -402,7 +511,45 @@ moveBall PROC uses ax
 	neg ball.SpeedX
 	ret
 	
+	youDied:
+	dec player.Lives
+	neg ball.SpeedY
+	ret
+	
+	
 moveBall endp
+;---------------------------------------------------------------------------------------------
+drawBrick proc uses ax bx cx dx si di
+	mov bh, 0; page number
+	mov si, 0
+	mov al, 1
+brickLoop_1:
+		mov cx, brick[si].CoordX ;inital x
+		mov dx, brick[si].CoordY ;inital y
+		mov brickLoopVar, 20
+		brickLoop_2:
+			mov di, brickLength
+			mov cx, brick[si].CoordX
+			brickLoop_3:
+				call drawPixel
+				inc cx
+				
+			dec di
+			cmp di, 0
+			jne brickLoop_3
+			
+			inc dx
+		dec brickLoopVar
+		cmp brickLoopVar, 0
+		jne brickLoop_2
+
+inc al
+add si, type BrickStruct
+cmp si, 60
+jb brickLoop_1
+ret
+drawBrick ENDP
+
 ;---------------------------------------------------------------------------------------------
 brickCollision proc uses ax bx cx dx di si
 	; ball.x < brick.x +brickLength
@@ -411,6 +558,7 @@ brickCollision proc uses ax bx cx dx di si
 	; ball.y + ballWidth > brick.y 
 	
 	mov si, -4
+	mov di, 0
 collisionLoop:
 	add si, 4
 	cmp si, 60
@@ -447,30 +595,19 @@ collisionLoop:
 	cmp al, 0; 0 for black colour
 	je collisionExit ; if the pixel is already black, skip the clearing
 	
-	
+	mov ax, si
+	checkForBrokenBrick:
+		cmp brokenBricks[di], ax  ; if its already inside the broken bricks array, skip over it
+		je collisionExit
+	inc di
+	cmp di, 15
+	jne checkForBrokenBrick
+		
+	addBrokenBrick si ; ax = si	(else add it to the array list)
 	clearBrick si
 	inc player.Score
 	;---------reflection off of a brick
-	;if collide from above or below
-		
-	;mov ax, ball.CoordY
-;	add ax, ballSize
-	;cmp ax, brick[si].CoordY
-	;jle reflectCheck
-	
-	
-;	mov ax, brick[si].CoordY
-	;add ax, brickWidth
-	;cmp ball.CoordY, ax
-	;jl reflectSideways
-	
 	neg ball.SpeedY
-;	jmp collisionExit
-	;else if collide from left or right
-	;reflectSideways:
-	;neg ball.SpeedX
-	
-	
 	
 	collisionExit:
 ret
@@ -501,9 +638,9 @@ drawBall proc uses ax bx cx dx si di
 ret
 drawBall endp
 ;------------------------
-displayScore Proc uses ax bx cx dx
+updateStats Proc uses ax bx cx dx
 	OUTP:
-	MOV AX, player.Score
+	MOV AX, statsVar
 	MOV DX,0
 
 	HERE:
@@ -533,5 +670,5 @@ displayScore Proc uses ax bx cx dx
 	JMP DISP
 	Ex2:
 ret
-displayScore endp
+updateStats endp
 end
